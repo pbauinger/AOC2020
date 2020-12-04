dade8194b3f8d3833cf2a25a80c8cfd21fc1ef13
Philipp Bauinger <philipp.bauinger@gmail.com>
2020-12-04 09:02:37 +0100

    Day4

--- src/Day4.kt
+++ src/Day4.kt
@@ -1,9 +1,46 @@
 fun main() {
-    val lines = readLines("day3/input.in")
-    println(cntT(lines, 3, 1))
-    println(cntT(lines, 1, 1) * cntT(lines, 3, 1) * cntT(lines, 5, 1) * cntT(lines, 7, 1) * cntT(lines, 1, 2))
+    val lines = readText("day4/input.in").split("\n\n").map { it.replace("\n", " ") }
+    println(part1(lines))
+    println(part2(lines));
 }
 
-private fun cntT(lines: List<String>, dc: Int, dr: Int): Long {
-    return lines.filterIndexed { i, v -> v[((i / dr) * dc) % v.length] == '#' && i % dr == 0 }.count().toLong()
+private fun part1(lines: List<String>): Int {
+    val required = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    var validCnt = 0
+    for(line in lines) {
+        val curr = required.toMutableList()
+        val found = line.split(" ").map { it.split(":").first() }
+        curr.removeAll(found)
+        if(curr.size == 0) validCnt++
+    }
+    return validCnt
+}
+
+private fun part2(lines: List<String>): Int {
+    var validCnt = 0
+    for(line in lines) {
+        var cnt = 0
+        for(part in line.split(" ")) {
+            if(part.split(":").size < 2) continue
+            val (property, value) = part.split(":")
+
+            when(property) {
+                "byr" -> if (value.toInt() in 1920..2002) cnt++
+                "iyr" -> if (value.toInt() in 2010..2020) cnt++
+                "eyr" -> if (value.toInt() in 2020..2030) cnt++
+                "hcl" -> if(value.length == 7 && Regex("#[0-9a-fA-F]+").matches(value)) cnt++
+                "ecl" -> if(value in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")) cnt++
+                "pid" -> if(value.length == 9 && value.toIntOrNull() != null) cnt++
+                "hgt" -> {
+                    Regex("(\\d+)(cm|in)").matchEntire(value)?.let {
+                        val(height, unit) = it.destructured
+                        if (unit == "cm" && height.toInt() in 150..193) cnt++
+                        if (unit == "in" && height.toInt() in 59..76) cnt++
+                    }
+                }
+            }
+        }
+        if(cnt==7) validCnt++
+    }
+    return validCnt
 }
--- src/FileUtils.kt
+++ src/FileUtils.kt
@@ -1,1 +1,3 @@
 fun readLines(file: String): List<String> = {}.javaClass.getResource(file).readText().lines()
+
+fun readText(file: String): String = {}.javaClass.getResource(file).readText()
