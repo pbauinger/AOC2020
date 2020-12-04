f0e513e4c116d34037eb897feb76fb8d9839c31b
Philipp Bauinger <philipp.bauinger@gmail.com>
2020-12-04 09:01:49 +0100

    Added day4

---
+++ src/Day4.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val lines = readLines("day3/input.in")
+    println(cntT(lines, 3, 1))
+    println(cntT(lines, 1, 1) * cntT(lines, 3, 1) * cntT(lines, 5, 1) * cntT(lines, 7, 1) * cntT(lines, 1, 2))
+}
+
+private fun cntT(lines: List<String>, dc: Int, dr: Int): Long {
+    return lines.filterIndexed { i, v -> v[((i / dr) * dc) % v.length] == '#' && i % dr == 0 }.count().toLong()
+}
