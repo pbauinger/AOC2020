2e6c4ec6b25f1bc9a1d8e60326bb64f54d7e6574
Philipp Bauinger <philipp.bauinger@gmail.com>
2020-12-06 13:05:37 +0100

    minor change

--- src/Day6.kt
+++ src/Day6.kt
@@ -2,7 +2,7 @@ fun main() {
     val groups = readText("day6/input.in").split("\n\n")
     val part1 = groups.sumBy { it.replace("\n", "").toSet().count() }
 
-    val part2 = groups.map { it.split("\n").map { it.toCharArray().toSet() } }.sumBy {
+    val part2 = groups.map { it.split("\n").map { it.toSet() } }.sumBy {
         it.fold(it.first()) { acc, i -> acc.intersect(i) }.size
     }
     println("$part1, $part2")
