7f33503d51520edbdfd439c4dbeca118741ea2e4
Philipp Bauinger <philipp.bauinger@gmail.com>
2020-12-06 13:14:40 +0100

    replaced fold with reduce

--- src/Day6.kt
+++ src/Day6.kt
@@ -3,7 +3,7 @@ fun main() {
     val part1 = groups.sumBy { it.replace("\n", "").toSet().count() }
 
     val part2 = groups.map { it.split("\n").map { it.toSet() } }.sumBy {
-        it.fold(it.first()) { acc, i -> acc.intersect(i) }.size
+        it.reduce { acc, set ->  acc.intersect(set)}.size
     }
     println("$part1, $part2")
 }
